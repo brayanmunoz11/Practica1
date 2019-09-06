5c705d472db7259f6ffa87382bb8896dce458462
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2019-09-06 12:26:36 -0500

    first commit

---
+++ hola mundo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    for(n>0)
+
+
+   system("pause");    return(0); }
---
+++ suma.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    float a,b, suma;
+    printf(" Ingrese primer numero:");
+    scanf("%f",&a);
+    printf(" Ingrese segundo numero:");
+    scanf("%f",&b);
+    suma=a+b;
+    printf("La suma es: %.2f \ ", suma);
+
+ system("pause");
+ return(0);
+}
